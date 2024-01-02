2483d3d5561b6aeff47162d12fbb762923e4af81
SAKSHI3529 <sakshi3529@gamil.com>
2024-01-02 16:52:48 +0530

    👀

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -17,49 +17,10 @@
                 </li>
                 
 
-                
-
-                {{-- <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="mdi mdi-email-outline"></i>
-                        <span>Email</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="email-inbox.html">Inbox</a></li>
-                        <li><a href="email-read.html">Email Read</a></li>
-                        <li><a href="email-compose.html">Email Compose</a></li>
-                    </ul>
-                </li> --}}
 
                 <li class="menu-title">Components</li>
 
-                {{-- <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="mdi mdi-buffer"></i>
-                        <span>UI Elements</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                            <li><a href="ui-alerts.html">Alerts</a></li>
-                            <li><a href="ui-buttons.html">Buttons</a></li>
-                            <li><a href="ui-badge.html">Badge</a></li>
-                            <li><a href="ui-cards.html">Cards</a></li>
-                            <li><a href="ui-carousel.html">Carousel</a></li>
-                            <li><a href="ui-dropdowns.html">Dropdowns</a></li>
-                            <li><a href="ui-grid.html">Grid</a></li>
-                            <li><a href="ui-images.html">Images</a></li>
-                            <li><a href="ui-lightbox.html">Lightbox</a></li>
-                            <li><a href="ui-modals.html">Modals</a></li>
-                            <li><a href="ui-pagination.html">Pagination</a></li>
-                            <li><a href="ui-popover-tooltips.html">Popover &amp; Tooltips</a></li>
-                            <li><a href="ui-rangeslider.html">Range Slider</a></li>
-                            <li><a href="ui-session-timeout.html">Session Timeout</a></li>
-                            <li><a href="ui-progressbars.html">Progress Bars</a></li>
-                            <li><a href="ui-sweet-alert.html">Sweet-Alert</a></li>
-                            <li><a href="ui-tabs-accordions.html">Tabs &amp; Accordions</a></li>
-                            <li><a href="ui-typography.html">Typography</a></li>
-                            <li><a href="ui-video.html">Video</a></li>
-                    </ul>
-                </li> --}}
+              
 
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect" aria-expanded="false">
@@ -105,123 +66,6 @@
                     </a>
                 </li>
 
-                {{-- <li>
-                    <a href="{{url('/reports/create')}}" class="waves-effect">
-                        <i class="mdi mdi-view-dashboard"></i>
-                        <span>Reports</span>
-                    </a>
-                </li> --}}
-
-
-{{-- 
-
-
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="mdi mdi-format-list-bulleted-type"></i>
-                        <span>Tables</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="tables-basic.html">Basic Tables</a></li>
-                        <li><a href="tables-datatable.html">Data Table</a></li>
-                        <li><a href="tables-responsive.html">Responsive Table</a></li>
-                        <li><a href="tables-editable.html">Editable Table</a></li>
-                    </ul>
-                </li>
-
-       
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="mdi mdi-album"></i>
-                        <span>Icons</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="icons-material.html">Material Design</a></li>
-                        <li><a href="icons-ion.html">Ion Icons</a></li>
-                        <li><a href="icons-fontawesome.html">Font Awesome</a></li>
-                        <li><a href="icons-themify.html">Themify Icons</a></li>
-                        <li><a href="icons-dripicons.html">Dripicons</a></li>
-                        <li><a href="icons-typicons.html">Typicons Icons</a></li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="waves-effect">
-                        <span class="badge badge-pill badge-danger float-right">2</span>
-                        <i class="mdi mdi-google-maps"></i>
-                        <span>Maps</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="maps-google.html"> Google Map</a></li>
-                        <li><a href="maps-vector.html"> Vector Map</a></li>
-                    </ul>
-                </li>
-
-                <li class="menu-title">Extras</li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="mdi mdi-responsive"></i>
-                        <span> Layouts </span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="layouts-horizontal.html">Horizontal</a></li>
-                        <li><a href="layouts-light-sidebar.html">Light Sidebar</a></li>
-                        <li><a href="layouts-compact-sidebar.html">Compact Sidebar</a></li>
-                        <li><a href="layouts-icon-sidebar.html">Icon Sidebar</a></li>
-                        <li><a href="layouts-boxed.html">Boxed Layout</a></li>
-                        <li><a href="layouts-preloader.html">Preloader</a></li>
-                        <li><a href="layouts-colored-sidebar.html">Colored Sidebar</a></li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="mdi mdi-account-box"></i>
-                        <span> Authentication </span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="pages-login.html">Login</a></li>
-                        <li><a href="pages-register.html">Register</a></li>
-                        <li><a href="pages-recoverpw.html">Recover Password</a></li>
-                        <li><a href="pages-lock-screen.html">Lock Screen</a></li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="mdi mdi-google-pages"></i>
-                        <span> Extra Pages </span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="pages-timeline.html">Timeline</a></li>
-                        <li><a href="pages-invoice.html">Invoice</a></li>
-                        <li><a href="pages-directory.html">Directory</a></li>
-                        <li><a href="pages-blank.html">Blank Page</a></li>
-                        <li><a href="pages-404.html">Error 404</a></li>
-                        <li><a href="pages-500.html">Error 500</a></li>
-                    </ul>
-                </li>
-
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="mdi mdi-share-variant"></i>
-                        <span>Multi Level</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="true">
-                        <li><a href="javascript: void(0);">Level 1.1</a></li>
-                        <li><a href="javascript: void(0);" class="has-arrow">Level 1.2</a>
-                            <ul class="sub-menu" aria-expanded="true">
-                                <li><a href="javascript: void(0);">Level 2.1</a></li>
-                                <li><a href="javascript: void(0);">Level 2.2</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-
-            </ul> --}}
         </div>
         <!-- Sidebar -->
     </div>
