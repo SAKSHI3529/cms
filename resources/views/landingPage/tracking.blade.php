08240058e01646a06a63f009708c411942d1e05b
SAKSHI3529 <sakshi3529@gamil.com>
2024-02-29 20:25:46 +0530

    order form editd

--- resources/views/landingPage/tracking.blade.php
+++ resources/views/landingPage/tracking.blade.php
@@ -278,6 +278,61 @@
 
   </main>
 
+  {{-- <footer id="footer" class="footer">
+
+    <div class="container footer-top">
+        <div class="row gy-4">
+            <div class="col-lg-5 col-md-12 footer-about">
+                <a href="index.html" class="logo d-flex align-items-center">
+                    <span>Madhur</span>
+                </a>
+                
+                <div class="social-links d-flex mt-4">
+                    <a href=""><i class="bi bi-twitter"></i></a>
+                    <a href=""><i class="bi bi-facebook"></i></a>
+                    <a href=""><i class="bi bi-instagram"></i></a>
+                    <a href=""><i class="bi bi-linkedin"></i></a>
+                </div>
+            </div>
+
+            <div class="col-lg-2 col-6 footer-links">
+                <h4>Useful Links</h4>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Terms of service</a></li>
+                    <li><a href="#">Privacy policy</a></li>
+                </ul>
+            </div>
+
+           
+
+            <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+                <h4>Contact Us</h4>
+                <p>A108 Adam Street</p>
+                <p>New York, NY 535022</p>
+                <p>United States</p>
+                <p class="mt-4"><strong>Phone:</strong> <span>+91 8659412358</span></p>
+                <p><strong>Email:</strong> <span>madhur@gmail.com</span></p>
+            </div>
+
+        </div>
+
+
+</footer>
+
+<!-- Scroll Top Button -->
+<a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
+        class="bi bi-arrow-up-short"></i></a>
+
+<!-- Preloader -->
+<div id="preloader">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</div> --}}
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
 
